06095b42f93385f8e1ac3aa580d8c3b787743687
Kevin Stradtman <stradtkt22@gmail.com>
2019-12-12 10:31:51 -0500

    Added key events

--- js/modules/search.js
+++ js/modules/search.js
@@ -6,26 +6,36 @@ class Search {
         this.closeButton = $('.search-overlay__close');
         this.searchOverlay = $('.search-overlay');
         this.events();
+        this.isOverlayOpen = false;
     }
 
     events() {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
-        $(document).on("keyUp", this.keyPressDispatcher.bind(this));
+        $(document).on("keyup", this.keyPressDispatcher.bind(this));
     }
 
-    keyPressDispatcher() {
-        console.log("This is a test");
+    keyPressDispatcher(e) {
+        // console.log(e.keyCode);
+        if(e.keyCode == 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+            
+        }
+        if(e.keyCode == 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
     }
 
     openOverlay() {
         this.searchOverlay.addClass('search-overlay--active');
         $('body').addClass('body-no-scroll');
+        this.isOverlayOpen = true;
     }
 
     closeOverlay() {
         this.searchOverlay.removeClass('search-overlay--active');
         $('body').removeClass('body-no-scroll');
+        this.isOverlayOpen = false;
     }
 }
 
